fef080ecaf424c52b4a9577b841454e85328c921
delead <einoeden@posteo.net>
2017-09-30 02:26:07 +0200

    some update in the modell struckture

--- solawi/models.py
+++ solawi/models.py
@@ -16,19 +16,28 @@ class User(AbstractUser):
     ''' '''
     is_member = models.BooleanField(_('Make a paying member'), default=True)
     is_supervisor = models.BooleanField(_('Make a depot supervisor'),
-                                        default=False)
+            default=False)
 
     depot = models.ForeignKey('Depot', on_delete=models.CASCADE,
-                              related_name='members', blank=True, null=True)
-    weeklybasket = models.ForeignKey('WeeklyBasket', on_delete=models.CASCADE,
-                                     related_name='members', blank=True,
-                                     null=True)
+            related_name='members', blank=True, null=True)
+    defaultbasket = models.ForeignKey('OrderBasket', on_delete=models.CASCADE,
+            related_name='members', blank=True,
+            null=True)
+    count_shares = models.IntegerField(null=True, blank=True, default=0,
+            validators=[validators.MinValueValidator(0)])
+
     assets = models.IntegerField(null=True, blank=True, default=0,
-                                 validators=[validators.MinValueValidator(0)])
-    account = models.TextField(null=True, blank=True, default='[]',
-                               help_text=_('Containing the JSON array of '
-                                           'this users gained potentials'),
-                               validators=[portion_account_validate])
+            validators=[validators.MinValueValidator(0)])
+    #    account = models.TextField(null=True, blank=True, default='[]',
+#                               help_text=_('Containing the JSON array of '
+#                                           'this users gained potentials'),
+#                               validators=[portion_account_validate])
+
+    momentary_order = models.ForeignKey('WeeklyBasket',
+            on_delete=models.CASCADE,
+            related_name='members',
+            blank=True,
+            null=True)
 
     class Meta:
         ''' '''
@@ -55,40 +64,58 @@ class User(AbstractUser):
         if self.is_member:
             if self.depot is None:
                 raise ValidationError(_('A Member has to have an depot.'))
-            if self.weeklybasket is None:
-                raise ValidationError(_('A Member has to have an '
-                                        'weekly basket.'))
+        if self.defaultbasket is None:
+            raise ValidationError(_('A Member has to have an '
+                                    'weekly basket.'))
 
-    def save(self, *args, **kwargs):
-        '''
+#    def save(self, *args, **kwargs):
+#        '''
+#
+#        Args:
+#          *args:
+#          **kwargs:
+#
+#        Returns:
+#
+#        '''
+#        if self.account:
+#            self.assets = 0
+#            this_week = utils.date_from_week()
+#            valid_days = settings.WEEKS_TO_SAVE_ACCOUNTS * 7
+#            for (year, week, asset) in json.loads(self.account):
+#                date_delta = this_week - utils.date_from_week(year, week)
+#                if date_delta.days <= valid_days:
+#                    self.assets += asset
+#        super().save(*args, **kwargs)
 
-        Args:
-          *args:
-          **kwargs:
 
-        Returns:
+class ProductProperty(models.Model):
+    product = models.ForeignKey('Product',
+            on_delete=models.CASCADE, related_name='productproperties')
 
-        '''
-        if self.account:
-            self.assets = 0
-            this_week = utils.date_from_week()
-            valid_days = settings.WEEKS_TO_SAVE_ACCOUNTS * 7
-            for (year, week, asset) in json.loads(self.account):
-                date_delta = this_week - utils.date_from_week(year, week)
-                if date_delta.days <= valid_days:
-                    self.assets += asset
-        super().save(*args, **kwargs)
+    packagesize = models.IntegerField()
+    unit = models.CharField(max_length=15,
+                            help_text=_('The unit to measure this food in, '
+                                        'e.g. kg or L'))
+    producttype = models.CharField(max_length=12,
+            help_text=_('Type of the product'))
+
+    def __str__(self):
+        pass
 
 
 class Product(models.Model):
     ''' '''
     name = models.CharField(max_length=30, unique=True)
-    unit = models.CharField(max_length=15,
-                            help_text=_('The unit to measure this food in, '
-                                        'e.g. kg or L'))
-    price = models.FloatField(
+    orderable = models.BooleanField(default=False)
+    
+    # default value none means not modular
+    modular = ( models.IntegerField(), models.FloatField( help_text=_('The price of the modular product.')))
+    # 0 means not exchangable
+    exchange_value = models.FloatField(null=True, blank=True, default=0,
         validators=[validators.MinValueValidator(0)],
-        help_text=_('The price per unit.'))
+        help_text=_('The exchange value per unit.'))
+
 
     class Meta:
         ''' '''
@@ -97,47 +124,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Portion(models.Model):
-    ''' '''
-    food = models.ForeignKey('Product', on_delete=models.CASCADE,
-                             related_name='portions')
-    quantity = models.IntegerField()
-    price = models.FloatField(
-        validators=[validators.MinValueValidator(0)],
-        help_text=_('The price of this Portion.'),
-        default=0)
-    # quantity = models.FloatField(
-    #     validators=[validators.MinValueValidator(0)],
-    #     help_text=_('The quantity of this Portion.'))
-
-    class Meta:
-        ''' '''
-        verbose_name = _('portion')
-        verbose_name_plural = _('portions')
-
-    def __str__(self):
-        return _('{quantity}{unit} of {food}').format(
-            quantity=self.quantity, unit=self.food.unit, food=self.food)
-
-    def get_price(self):
-        ''' '''
-        return self.quantity * self.food.price
-
-    def save(self, *args, **kwargs):
-        '''
-
-        Args:
-          *args:
-          **kwargs:
-
-        Returns:
-
-        '''
-        self.price = self.get_price()
-        super().save(*args, **kwargs)
-
 
 class Depot(models.Model):
     ''' '''
@@ -153,41 +139,34 @@ class Depot(models.Model):
         return _('{name} at {location}').format(name=self.name,
                                                 location=self.location)
 
-
-class WeeklyBasket(models.Model):
-    ''' '''
-    name = models.CharField(max_length=50)
-    contents = models.ManyToManyField('Portion')
-
-    class Meta:
-        ''' '''
-        verbose_name = _('weekly basket')
-        verbose_name_plural = _('weekly baskets')
-
-    def __str__(self):
-        cstr = ', '.join([str(i) for i in self.contents.all()])
-        return _('{name}: {contents}').format(name=self.name,
-                                              contents=cstr)
-
-
-class OrderBasketProduct(models.Model):
-    portion = models.ForeignKey('Portion')
-    basket = models.ForeignKey('OrderBasket')
+class ProductAmountPerOrder(models.Model):
+    product = models.ForeignKey('Product')
+    order = models.ForeignKey('OrderBasket')
     count = models.IntegerField(default=0)
 
     def __str__(self):
         week = self.basket.week.strftime('%W')
         year = self.basket.week.year
-        return '{count} of {portion} for {user} in {year}-{week}'.format(
-            count=self.count, portion=self.portion, user=self.basket.user,
+        return '{count} of {product} for {user} in {year}-{week}'.format(
+            count=self.count, product=self.product, user=self.basket.user,
             year=year, week=week)
+
+    class Meta:
+        ''' '''
+        verbose_name = _('PAPO')
+        verbose_name_plural = _('PAPOs')
+
+    def __str__(self):
+        return _('{name} at {location}').format(name=self.name,
+                                                location=self.location)
 
 class OrderBasket(models.Model):
     ''' '''
     week = models.DateField()
-    user = models.ForeignKey('User', on_delete=models.CASCADE,
-                             related_name='orders')
-    contents = models.ManyToManyField('Portion', through='OrderBasketProduct')
+    # uniquenes of the order?
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='orders')
+    contents = models.ManyToManyField('Product', through='ProductAmountPerOrder')
+
     edited_weekly_basket = models.BooleanField(
         _('Has edited the weekly basket for this week'), default=False)
 
@@ -222,3 +201,8 @@ class OrderBasket(models.Model):
         year = self.week.year
         return _('{year}-{week} by {user}: {contents}').format(
             year=year, week=week, user=self.user, contents=ostr)
+
+
+class WeeklyBasket(models.Model):
+    ''' '''
+    order = OrderBasket()
