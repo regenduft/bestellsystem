d4aa2717362b4d9d1ecc3a3e6eab66e4ed12381e
delead <einoeden@posteo.net>
2018-02-06 02:07:13 +0100

    Applied changes of datestructure to isoformat

--- solawi/signals.py
+++ solawi/signals.py
@@ -6,54 +6,35 @@ from django.db.models import Prefetch
 from django.dispatch import receiver
 from solawi import OrderBasket, User
 
-@receiver(post_save, sender=OrderBasket)
+@receiver(post_save, sender=OrderBasket, dispatch_uid="createorder")
 def post_create_orderbasket(sender, instance, created, **kwargs):
     if created:
-        defaults = instance.user.defaultbasket.content.contains.filter(productproperty__orderable=True,
-                productproperty__product__orderable=True).prefetch_related('productproperty__product','count')
+        
+        instance.content = OrderContent.objects.create()
+        instance.save()
+
         # take defaultBasket of user in
+        defaults = instance.user.defaultbasket.content.contains.filter(productproperty__orderable=True,
+                productproperty__product__orderable=True).prefetch_related('productproperty__product')
         for amount in defaults:
             instance.content.add_or_create_product(amount.productproperty, amount.count)
 
-        # substract deorders of user and account to user
+        # get counterorders of user
         counters = instance.user.regularyorders.filter(productproperty__in=[a.productproperty
-                for a in defaults],
-                is_counterorder=True).prefetch_related('productproperty__product','count')
-
-        #income = 0
+                for a in defaults], is_counterorder=True).prefetch_related('productproperty__product')
+        # substract deorders of user and account to user
         for each in counters:
-            instance.sub_from_order(each.productproperty, each.count)         
-            #TODO UPDATE last order
-            # old with savings       
-            # value, existed = instance.content.sub_or_delete_product(amount.productproperty,
-            #                                                         amount.count)         
-            # if existed:
-               # income += value
-        counters.update(lastorder = datetime.date.today())
+            instance.sub_from_order(each.productproperty, -each.count)         
 
-        # distribute savings on users Regular orders
-        regords = instance.user.regularyorders.filter(productproperty__orderable=True,
+        # update lastorder date
+        counters.update(lastorder_isoweek=instance.isoweek, lastorder_isoyear=instance.isoyear)
+
+        # fetch regulary orders which are orderable, ready and not counterorders
+        regords = instance.user.regularyorders.select_for_update().filter(productproperty__orderable=True,
                                                       productproperty__product__orderable=True,
-                                                      is_counterorder=False).prefetch_related('productproperty__product','count')
+                                                      is_ready_order=True).prefetch_related('productproperty__product','count')
 
+        # add regords of user and account to user
         for each in regords:
             instance.add_to_order(each.productproperty, each.count)         
-            #TODO UPDATE last order
-        regords.update(lastorder = datetime.date.today())
-
-# old savings version
-#        leftout = income
-#        for reg in regords:
-#            if reg.ready:
-#                pass
-#            else:
-#                saved = min(reg.current_counterorder_share(regords)*income,
-#                        reg.exchange_value/period)
-#                reg.savings += saved
-#                leftout -= saved
-#            # account regular orders in if ready
-#            if reg.orderable:
-#                instance.content.add_or_create_product(reg.productproperty, reg.count)
-#
-#        # add leftout to Users assets
-#        instance.user.assets += leftout
+        regords.update(lastorder_isoweek=instance.isoweek, lastorder_isoyear=instance.isoyear)
